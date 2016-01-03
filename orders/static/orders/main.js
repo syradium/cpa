adef01a413ac0343ddc680fd9ff0892f6b015de8
sysradium <sysradium@users.noreply.github.com>
2016-01-03 23:52:03 +0300

    Switch to russian language by default

--- orders/static/orders/main.js
+++ orders/static/orders/main.js
@@ -1,19 +1,19 @@
 $(function() {
 	var order_columns  = [
 	{field: 'order_id', title: 'ID'},
-	{formatter: dateFormatter, title: 'Created on'},
-	{field: 'domain', title: 'Domain'},
-	{field: 'price', title: 'Price'},
-	{field: 'data', formatter: dataFormatter('status'), title: 'Status'},
-	{field: 'data', formatter: dataFormatter('name'), title: 'Name'},
-	{field: 'data', formatter: dataFormatter('phone'), title: 'Phone'},
-	{field: 'data', formatter: dataFormatter('payment_sum'), title: 'Pay sum'},
-	{field: 'data', formatter: dataFormatter('payment_status'), title: 'Pay status'},
-	{field: 'data', formatter: dataFormatter('utm1'), title: 'Utm_source'},
-	{field: 'data', formatter: dataFormatter('utm2'), title: 'Utm_campaign'},
-	{field: 'data', formatter: dataFormatter('utm3'), title: 'Utm_content'},
-	{field: 'data', formatter: dataFormatter('utm4'), title: 'Utm_term'},
-	{events: 'window.operateEvents', formatter: actionsFormatter, title: 'Actions'},
+	{formatter: dateFormatter, title: 'Дата'},
+	{field: 'domain', title: 'Домен'},
+	{field: 'price', title: 'Цена'},
+	{field: 'data', formatter: dataFormatter('status'), title: 'Статус'},
+	{field: 'data', formatter: dataFormatter('name'), title: 'Имя'},
+	{field: 'data', formatter: dataFormatter('phone'), title: 'Телефон'},
+	{field: 'data', formatter: dataFormatter('payment_sum'), title: 'Сумма оплаты'},
+	{field: 'data', formatter: dataFormatter('payment_status'), title: 'Статус оплаты'},
+	{field: 'data', formatter: dataFormatter('utm1'), title: 'Utm source'},
+	{field: 'data', formatter: dataFormatter('utm2'), title: 'Utm campaign'},
+	{field: 'data', formatter: dataFormatter('utm3'), title: 'Utm content'},
+	{field: 'data', formatter: dataFormatter('utm4'), title: 'Utm term'},
+	{events: 'window.operateEvents', formatter: actionsFormatter, title: 'Действия'},
 	];
 
 	window.operateEvents = {
@@ -36,7 +36,7 @@ $(function() {
 });
 
 function actionsFormatter(row, value) {
-	return '<a href="#" class="action remove" ic-delete-from="/api/orders/' + value.pk + '/">Delete</a>'
+	return '<a href="#" class="action remove" ic-delete-from="/api/orders/' + value.pk + '/">Удалить</a>'
 }
 
 function dataFormatter(field) {
