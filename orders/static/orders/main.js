c1bd6cf8a3e2ca1742e4ab18ec5b3e65103b38a7
sysradium <sysradium@users.noreply.github.com>
2016-01-03 18:01:46 +0300

    Add statistics table to order list

--- orders/static/orders/main.js
+++ orders/static/orders/main.js
@@ -1,19 +1,22 @@
 $(function() {
-	var columns  = [
-        {field: 'order_id', title: 'Order ID'},
-        {field: 'created_on', formatter: dateFormatter, title: 'Created on'},
-        {field: 'domain', title: 'Domain'},
-        {field: 'price', title: 'Price'},
-        {field: 'data', formatter: dataFormatter('status'), title: 'Status'},
-        {field: 'data', formatter: dataFormatter('payment_status'), title: 'Payment status'},
-        {field: 'data', formatter: dataFormatter('utm1'), title: 'Utm_source'},
-        {field: 'data', formatter: dataFormatter('utm2'), title: 'Utm_campaign'},
-        {field: 'data', formatter: dataFormatter('utm3'), title: 'Utm_content'},
-        {field: 'data', formatter: dataFormatter('utm4'), title: 'Utm_term'},
-        {events: 'window.operateEvents', formatter: actionsFormatter, title: 'Actions'},
+	var order_columns  = [
+	{field: 'order_id', title: 'ID'},
+	{formatter: dateFormatter, title: 'Created on'},
+	{field: 'domain', title: 'Domain'},
+	{field: 'price', title: 'Price'},
+	{field: 'data', formatter: dataFormatter('status'), title: 'Status'},
+	{field: 'data', formatter: dataFormatter('name'), title: 'Name'},
+	{field: 'data', formatter: dataFormatter('phone'), title: 'Phone'},
+	{field: 'data', formatter: dataFormatter('payment_sum'), title: 'Pay sum'},
+	{field: 'data', formatter: dataFormatter('payment_status'), title: 'Pay status'},
+	{field: 'data', formatter: dataFormatter('utm1'), title: 'Utm_source'},
+	{field: 'data', formatter: dataFormatter('utm2'), title: 'Utm_campaign'},
+	{field: 'data', formatter: dataFormatter('utm3'), title: 'Utm_content'},
+	{field: 'data', formatter: dataFormatter('utm4'), title: 'Utm_term'},
+	{events: 'window.operateEvents', formatter: actionsFormatter, title: 'Actions'},
 	];
 
-    window.operateEvents = {
+	window.operateEvents = {
 		'click .remove': function (e, value, row, index) {
 			$order_list.bootstrapTable('removeByUniqueId', row.pk);
 			e.preventDefault();
@@ -21,14 +24,15 @@ $(function() {
 	};
 
 	var $order_list = $('#orderList').bootstrapTable({
-		columns: columns,
+		columns: order_columns,
 		pageSize: 25,
 		pageList: '[50, 100, 250, All]',
 		showRefresh: true
-    }).on('post-body.bs.table', function () {
+	}).on('post-body.bs.table', function () {
 		Intercooler.processNodes($('.action'));
 	});
 
+	var $statistics = $('#statistics').bootstrapTable();
 });
 
 function actionsFormatter(row, value) {
@@ -42,5 +46,40 @@ function dataFormatter(field) {
 }
 
 function dateFormatter(row, value) {
-	return moment().utcOffset(value).format('DD.MM.YY HH:MM');
+	return moment.utc(value.created_on).local().format('DD.MM.YY HH:MM');
+}
+
+function statisticsResponseHandler(res) {
+	response = {};
+	res.map(function(a) {
+		var date = extractDate(a.created_on);
+		if(!(date in response))
+			response[date] = {sum_paid: 0, sum_total: 0, total: 0, accepted: 0};
+
+		if(a.data.status !== undefined)
+		{
+			var x = response[date][a.data.status];
+			response[date][a.data.status] = (x === undefined) ? 1 : x + 1;
+		}
+		if(a.data.payment_sum !== undefined)
+			response[date].sum_paid += a.data.payment_sum;
+		response[date].sum_total += a.price;
+		response[date].total += 1;
+	});
+	return $.map(response, function(value, index) {
+		value['date'] = index;
+		return value;
+	});
+}
+
+function extractDate(timestamp) {
+	return moment.utc(timestamp).local().format('DD.MM.YYYY');
+}
+
+function priceFormatter(value, row) {
+	return value + ' ₽';
+}
+
+function approvedFormatter(value, row) {
+	return row.accepted / row.total * 100 + '%';
 }
